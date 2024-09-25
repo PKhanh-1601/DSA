b2b4f54322c59e7cfb1cc87fa4b66f95b84ad439
PhuongKhanh_1601 <22520639@gm.uit.edu.vn>
2024-09-25 15:04:46 +0700

    Update middlenode.cpp

--- middlenode.cpp
+++ middlenode.cpp
@@ -34,6 +34,7 @@ void append(Node*& head, int newData)
     last->next = newNode;
 }
 
+
 void printList(Node* head)
 {
     Node* temp = head;
@@ -48,18 +49,21 @@ void printList(Node* head)
 Node* findMiddle(Node* head)
 {
     if (head == NULL)
-    {
         return NULL;
-    }
-
-    Node* slow = head;
-    Node* fast = head;
-    while (fast != NULL && fast->next != NULL)
+    int count = 0;
+    Node* temp = head;
+    while (temp != NULL)
     {
-        slow = slow->next;
-        fast = fast->next->next;
+        count++;
+        temp = temp->next;
     }
-    return slow;
+    int middleIndex = count / 2;
+    temp = head;
+    for (int i = 0; i < middleIndex; i++)
+    {
+        temp = temp->next;
+    }
+    return temp;
 }
 
 int main()
@@ -81,7 +85,8 @@ int main()
     if (middle != NULL)
     {
         cout << "Phan tu giua cua danh sach: " << middle->data << endl;
-    } else
+    }
+    else
     {
         cout << "Danh sach lien ket rong." << endl;
     }
